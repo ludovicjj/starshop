48715deb68f3cb8b0ae661b7d6cc4baba08ec5f0
ludovicjj <jahanlud@gmail.com>
2024-11-16 11:20:50 +0100

    recipe flex && twig package

---
+++ src/Controller/MainController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function homepage(): Response
+    {
+        $starshipCount = 457;
+        $myShip = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'under construction',
+        ];
+
+        return $this->render('main/homepage.html.twig', parameters: [
+            'numberOfStarships' => $starshipCount,
+            'my_ship' => $myShip,
+        ]);
+    }
+}
